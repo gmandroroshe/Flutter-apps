e38c350068fc6ff03b98b96460f7607efe1d2aa0
Kavindu Maduranga <144511926+gmandroroshe@users.noreply.github.com>
2024-07-22 21:42:02 +0530

    black world project

---
+++ black_world/black_world/android/app/src/main/kotlin/com/example/black_world/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.black_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- noteapp/lib/main.dart
+++ noteapp/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'note_page.dart';
 import 'settings_page.dart'; // Import your SettingsPage
 
+
 void main() {
   runApp(MyApp());
 }
